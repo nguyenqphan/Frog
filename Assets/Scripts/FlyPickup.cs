928d63ee048380029a8c15920d6f3e02a2f3ccf7
Nguyen Phan <nqphan212@gmail.com>
2015-09-02 13:04:22 -0400

    Add particle effect to the fly when it is picked up. Also destroy the particle effect after 5 seconds

--- Assets/Scripts/FlyPickup.cs
+++ Assets/Scripts/FlyPickup.cs
@@ -3,10 +3,16 @@ using System.Collections;
 
 public class FlyPickup : MonoBehaviour {
 
+	[SerializeField]
+	private GameObject pickupPrefab;
+
 	void OnTriggerEnter(Collider other){
 
 		//if the collider other is tagged with "Player"..
 		if (other.CompareTag("Player")) {
+
+			//...add the pickup particles...
+			Instantiate(pickupPrefab, transform.position, Quaternion.identity);
 
 			Destroy(gameObject);
 		}
