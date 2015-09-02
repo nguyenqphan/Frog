719824addb9dd557a785ed0b7e22166f9558ed71
Nguyen Phan <nqphan212@gmail.com>
2015-09-02 15:30:58 -0400

    Add a bird to chase the frog

---
+++ Assets/Scripts/BirdMovement.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BirdMovement : MonoBehaviour {
+
+	[SerializeField]
+	private Transform target;
+	private NavMeshAgent birdAgent;
+	private Animator birdAnimator;
+
+	// Use this for initialization
+	void Start () {
+		birdAgent = GetComponent<NavMeshAgent>();
+		birdAnimator = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//...Set the bird's destination
+		birdAgent.SetDestination (target.position);
+
+		// Measure the magnitude of the NavMessAgent's velocity
+		float speed = birdAgent.velocity.magnitude;
+		
+		//pass the velocity to the animator component
+		birdAnimator.SetFloat("Speed", speed);
+	}
+
+
+
+}
